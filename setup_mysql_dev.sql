d4accc157e1fea839932a3d71a3fe207a526ff5d
root <a.kabasinga@alustudent.com>
2025-05-26 11:37:07 -0700

    setup db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
-
+-- A Script that prepares a MySQL serve for the project 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
